c33c4b7c3be070a652906f85bdf070f616d9c954
Albert Dayn <Albert.Dayn@Colorado.edu>
2019-03-21 01:40:46 -0600

    Added jupyter keybindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@
 set --export PATH ~/.cargo/bin $PATH
 set --export PATH ~/.local/bin $PATH
 set --export PATH $PATH '/usr/local/opt/coreutils/libexec/gnubin'
-set --export EDITOR "/usr/bin/env subl"
+set --export EDITOR "/usr/bin/env nano"
 set --export CLICOLOR 1
 set --export LSCOLORS 'HxfxfxfxexfxfxexexHxHx'
 
@@ -31,35 +31,47 @@ set fish_pager_color_progress		brwhite --bold
 set fish_greeting
 
 
-alias ls 'command ls -AFLh'
-alias ll 'ls -l'
+alias l 'command ls -AFLh'
+	alias ll 'l -l'
 alias rst 'exec fish'
 alias julia '/Applications/Julia-0.6.app/Contents/Resources/julia/bin/julia'
 
-alias cu "cd ~/college"
-alias ta "cd ~/college/ta"
 alias cdow "cd ~/Downloads"
-alias cu1 "echo \"NLP\"; cd ~/college/csci_5832/"
-alias cu2 "echo \"MechE\"; cd ~/college/mcen_4045/"
-alias cu3 "echo \"CV\"; cd ~/college/csci_5722/"
+alias cu "cd ~/college"
+	alias ta "cd ~/college/ta"
+		alias nbg "ssh nbg"
+		alias nbgmnt "sshfs nbg: ~/college/ta/nbg_mount_point/; cd ~/college/ta/nbg_mount_point/"
+		alias nbgunt "umount ~/college/ta/nbg_mount_point/"
+	alias cu1 "echo \"NLP\"; cd ~/college/csci_5832/"
+	alias cu2 "echo \"MechE\"; cd ~/college/mcen_4045/"
+	alias cu3 "echo \"CV\"; cd ~/college/csci_5722/"
 
 abbr --add ra trash
 abbr --add p 'python3'
 abbr --add link 'ln -s'
 abbr --add hi 'stack repl'
+abbr --add gc 'git commit'
+abbr --add ga 'git add'
+abbr --add gaa 'git add -A'
+abbr --add jn 'jupyter notebook'
+
+# Java
+set --export JAVA_8_HOME (/usr/libexec/java_home -v1.8)
+set --export JAVA_10_HOME (/usr/libexec/java_home -v10)
+set --export JAVA_11_HOME (/usr/libexec/java_home -v11)
+
+abbr --add java8 'set --export --universal JAVA_HOME $JAVA_8_HOME'
+abbr --add java11 'set --export --universal JAVA_HOME $JAVA_11_HOME'
+abbr --add java10 'set --export --universal JAVA_HOME $JAVA_10_HOME'
+abbr --add java_new 'java11'
 
 # PYENV
 set PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
 pyenv rehash
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/albertdayn/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/albertdayn/google-cloud-sdk/path.fish.inc'; else; . '/Users/albertdayn/google-cloud-sdk/path.fish.inc'; end; end
 
 clear
 
-# OPAM configuration
-# . /Users/albertdayn/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
-# ???? . /Users/albertdayn/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-# eval (opam config env)
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/albertdayn/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/albertdayn/google-cloud-sdk/path.fish.inc'; else; . '/Users/albertdayn/google-cloud-sdk/path.fish.inc'; end; end
