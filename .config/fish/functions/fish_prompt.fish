9999451907343a482c3b0a3dca1d4170384961c2
Albert Dayn <Albert.Dayn@Colorado.edu>
2020-06-03 18:51:46 +0000

    Added ssh handling to fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,14 @@
 function fish_prompt
+	if test -n "$SSH_CLIENT$SSH_TTY" # Check if either are populated
+		set prefix "("(set_color -o yellow)(hostname)(set_color -o normal)") "
+	else
+		set prefix ""
+	end
 	if test $status = 0
 		set color green
 	else
 		set color red
 	end
-	echo (set_color -o $color)'>>= '(set_color -o normal)
+
+	echo "$prefix"(set_color -o $color)'>>= '(set_color -o normal)
 end
