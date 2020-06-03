65268848815df32cc25d971e03d13f71546a705e
Albert Dayn <Albert.Dayn@Colorado.edu>
2020-06-03 18:53:51 +0000

    Fixed prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,10 +1,11 @@
 function fish_prompt
+	set last_stat "$status"
 	if test -n "$SSH_CLIENT$SSH_TTY" # Check if either are populated
 		set prefix "("(set_color -o yellow)(hostname)(set_color -o normal)") "
 	else
 		set prefix ""
 	end
-	if test $status = 0
+	if test $last_stat = 0
 		set color green
 	else
 		set color red
